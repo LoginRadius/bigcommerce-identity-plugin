b718684703c2472a5c1f1d64dcf48ab1bb422418
Karl-LoginRadius <feorn16@gmail.com>
2017-03-24 11:39:51 -0700

    Handle Checkout Redirect

--- blueprint-package/assets/js/loginradius/LoginRadiusInterface.js
+++ blueprint-package/assets/js/loginradius/LoginRadiusInterface.js
@@ -24,6 +24,17 @@ LoginRadius_Bigcommerce.util={};
         document.body.appendChild(js);
     };
 	
+	function getParameterByName(name, url) {
+		if (!url) {
+		  url = window.location.href;
+		}
+		name = name.replace(/[\[\]]/g, "\\$&");
+		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+			results = regex.exec(url);
+		if (!results) return null;
+		if (!results[2]) return '';
+		return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
 	
 	util.sendusertosite=function (url){
 		setTimeout(function(){  window.location=url}, 2000);
@@ -31,7 +42,13 @@ LoginRadius_Bigcommerce.util={};
 	
 	util.getURL = function (access_token,apikey,password,store)
 	{
-		var url="//cloud-api.loginradius.com/sso/bigcommerce/api/token?access_token="+access_token +"&apikey="+apikey +"&store="+store+"&password="+password;
+		
+		if(getParameterByName("return_url")){
+			var url="//cloud-api.loginradius.com/sso/bigcommerce/api/token?access_token="+access_token +"&apikey="+apikey +"&store="+store+"&password="+password+"&redirectto="+getParameterByName("return_url");
+		
+		}else{
+			var url="//cloud-api.loginradius.com/sso/bigcommerce/api/token?access_token="+access_token +"&apikey="+apikey +"&store="+store+"&password="+password;
+		}
 		return url;
 	}
 	
