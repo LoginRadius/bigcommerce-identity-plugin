182fd4655ff51fa456d89efd119a892344f34327
Karl-LoginRadius <feorn16@gmail.com>
2017-07-13 12:52:31 -0700

    Restructure folder

---
+++ bigcommerce-blueprint-package/assets/js/loginradius/LoginRadiusInterface.js
@@ -0,0 +1,383 @@
+
+	
+var LoginRadius_Bigcommerce = {};
+var $LRBC = LoginRadius_Bigcommerce;
+LoginRadius_Bigcommerce.util={};
+
+(function (util) {
+	
+	util.jsonpCall = function (path, handle) {
+        var func = 'Loginradius' + Math.floor((Math.random() * 1000000000000000000) + 1);
+        window[func] = function (data) {
+            handle(data);
+            try {
+                delete window[func];
+            }
+            catch (e) {
+                window[func] = undefined;
+            }
+            document.body.removeChild(js);
+        };
+        var js = document.createElement('script');
+        js.src = path +"&callback="+func;
+        js.type = "text/javascript";
+        document.body.appendChild(js);
+    };
+	
+	
+	util.sendusertosite=function (url){
+		setTimeout(function(){  window.location=url}, 2000);
+	};
+	
+	util.getURL = function (access_token,apikey,password,store)
+	{
+		var url="//cloud-api.loginradius.com/sso/bigcommerce/api/token?access_token="+access_token +"&apikey="+apikey +"&store="+store+"&password="+password;
+		return url;
+	}
+	
+	
+})(LoginRadius_Bigcommerce.util)
+
+LoginRadiusBCUX = (function (doc) {
+	var LRBCUX = {};
+	
+	LRBCUX.interface={};
+	
+	LRBCUX.interface.showMessage=function (msg,timeout){
+		 document.getElementById("lr-message-container").style.display = 'block';
+		 document.getElementById("lr-message-container").innerHTML=msg;
+		 setTimeout(function(){  document.getElementById("lr-message-container").style.display = 'none';}, timeout);
+		};
+		
+	LRBCUX.interface.definelogin=function (){
+			
+			var login_options = {};
+			login_options.onSuccess = function(response,data) {
+				var url=$LRBC.util.getURL(response.access_token,raasoption.apiKey ,data.password,storeName);
+				
+				LRBCUX.interface.showMessage("Login Successful, you will be redirected momentarily",5000);
+				
+				$LRBC.util.jsonpCall(url,function(tokendata){
+					
+						if(tokendata.loginUrl!=null)
+						{
+							$LRBC.util.sendusertosite(tokendata.loginUrl);
+						}else{
+							LRBCUX.interface.showMessage("Something went wrong during login please try again",5000);
+							document.getElementById("fade").style.display = 'none';
+						}
+					}); 
+				
+			};
+			
+			login_options.onError = function(errors) {
+				document.getElementById("fade").style.display = 'none';
+				if(errors.length && errors[0].Description!=null)
+					LRBCUX.interface.showMessage(errors[0].Description,5000);
+				
+			};
+			login_options.container = "login-div";
+			LRObject.init("login",login_options);
+
+		};
+	LRBCUX.interface.defineregister=function (){
+		
+		var registration_options = {}
+		
+			registration_options.onSuccess = function(response) {
+				// On Success
+				console.log(response);			
+				if(response!=null && response.IsPosted!=null && response.IsPosted==true)
+					LRBCUX.interface.showMessage("An email has been sent to your account, please click on the link to verify your Email",5000);
+				document.getElementById("fade").style.display = 'none';
+			};
+			
+			registration_options.onError = function(errors) {
+						
+				document.getElementById("fade").style.display = 'none';
+					if(errors.length && errors[0].Description!=null)
+						LRBCUX.interface.showMessage(errors[0].Description,5000);
+			};
+			
+			registration_options.container = "register-div";
+			
+			LRObject.init("registration",registration_options);	
+		
+		};
+	
+	LRBCUX.interface.definesocial=function (){
+
+		
+		LRObject.customInterface(".interfacecontainerdiv", raasoption);
+		
+		var sl_options = {};
+			sl_options.onSuccess = function(response) {
+			// On Success this callback will call
+			// response will be string as token
+			if(response.IsPosted){
+				document.getElementById("fade").style.display = 'none';
+				LRBCUX.interface.showMessage("An email has been sent to the provided email address",5000);
+			}
+			else{
+			var url=$LRBC.util.getURL(response.access_token,raasoption.apiKey ,"",storeName);
+			
+			LRBCUX.interface.showMessage("Login Successful, you will be redirected momentarily",5000);
+			
+			$LRBC.util.jsonpCall(url,function(data){
+				
+					if(data.loginUrl!=null)
+				{
+					$LRBC.util.sendusertosite(data.loginUrl);
+				}else{
+					LRBCUX.interface.showMessage("Something went wrong during login please try again",5000);
+					document.getElementById("fade").style.display = 'none';
+				}
+				});
+			}
+			
+			
+			
+			};
+			
+			sl_options.onError = function(errors) {
+				document.getElementById("fade").style.display = 'none';
+				if(errors.length && errors[0].Description!=null&& errors[0].ErrorCode!=905)
+					LRBCUX.interface.showMessage(errors[0].Description,5000);
+			};
+			sl_options.container = "sociallogin-container";
+		
+		LRObject.init('socialLogin', sl_options);
+		
+	};
+		
+	LRBCUX.interface.defineforgot = function (){
+		
+		var forgotpassword_options = {};
+		
+			forgotpassword_options.container = "forgotpassword-div";
+			
+			forgotpassword_options.onSuccess = function(response){
+
+				document.getElementById("fade").style.display = 'none';
+				LRBCUX.interface.showMessage("An Email link has been sent to the specified email. Click on the link to reset your password.",5000);
+			};
+			
+			forgotpassword_options.onError = function(errors) {
+
+				document.getElementById("fade").style.display = 'none';
+				if(errors.length && errors[0].Description!=null)
+					LRBCUX.interface.showMessage(errors[0].Description,5000);
+			};
+		LRObject.init("forgotPassword", forgotpassword_options);
+		
+		};
+	
+	LRBCUX.interface.definereset= function (){
+		
+		var resetpassword_options = {};
+		
+			resetpassword_options.container = "resetpassword-container";
+			
+			resetpassword_options.onSuccess =function(response) {
+
+			  LoginRadiusBCUX.interface.toggleform('login');
+				
+			  LRBCUX.interface.showMessage("Your new password has been set",5000);
+			  document.getElementById("fade").style.display = 'none';
+			};
+			
+			resetpassword_options.onError = function(errors) {
+				if(errors.length && errors[0].Description!=null)
+					LRBCUX.interface.showMessage(errors[0].Description,5000);
+				if(errors.length && errors[0].message!=null &&errors[0].id)
+					LRBCUX.interface.showMessage(errors[0].message,5000);
+				document.getElementById("fade").style.display = 'none';
+			};
+		var vtype = LRObject.util.getQueryParameterByName("vtype");	
+		if (vtype != null && vtype != "") {
+			if (vtype == "reset")
+				{
+					LRObject.init("resetPassword", resetpassword_options);
+				}
+			}
+		};
+	
+	LRBCUX.interface.defineverify=function (){
+		
+		var verifyemail_options = {};
+		
+			verifyemail_options.onSuccess = function(response) {
+				var locCheck=window.location.href
+				if(!locCheck.includes("vtype=reset")){
+					LRBCUX.interface.showMessage("Your email has been successfully verified.",5000);
+				}
+				document.getElementById("fade").style.display = 'none';
+			};
+			
+			verifyemail_options.onError = function(errors) {
+			  // error
+				if(errors.length && errors[0].Description!=null)
+				{
+					LRBCUX.interface.showMessage(errors[0].Description,5000);
+					document.getElementById("fade").style.display = 'none';
+				}
+			};
+		var vtype = LRObject.util.getQueryParameterByName("vtype");	
+		if (vtype != null && vtype != "") {
+			if (vtype == "emailverification")
+				{
+				
+					LRObject.init("verifyEmail", verifyemail_options);
+				}
+			}
+		
+	};
+		
+	//V2 Interfaces
+
+	LRBCUX.interface.definechangepassword=function (){
+		var changepassword_options = {};
+		
+			changepassword_options.container = "changepassword-container";
+			changepassword_options.onSuccess = function(response) {
+				
+				console.log(response);
+				LRBCUX.interface.showMessage("Password has been successfully updated",5000);
+			};
+			changepassword_options.onError = function(response) {
+				
+				LRBCUX.interface.showMessage(response[0].Description,5000);
+			};
+			LRObject.init("changePassword",changepassword_options);
+	};
+	LRBCUX.interface.defineprofileeditor=function (){
+		var profileeditor_options = {};
+			profileeditor_options.container = "profileeditor-container";
+			profileeditor_options.onSuccess = function(response) {
+	
+				console.log(response);
+				LRBCUX.interface.showMessage("Profile has been successfully updated",5000);
+			};
+			profileeditor_options.onError = function(response) {
+		
+				LRBCUX.interface.showMessage(response[0].Description,5000);
+			};
+		LRObject.init("profileEditor",profileeditor_options);
+
+	};	
+
+	LRBCUX.interface.defineaddemail=function (){
+		var addemail_options= {};
+			addemail_options.container = "addemail-container";
+			addemail_options.onSuccess = function(response) {
+			// On Success
+			console.log(response);
+			LRBCUX.interface.showMessage("Email has been successfully updated",5000);
+			};
+			addemail_options.onError = function(response) {
+			// On Error
+			LRBCUX.interface.showMessage(response[0].Description,5000);
+			};
+			LRObject.init("addEmail",addemail_options);
+	};
+	LRBCUX.interface.defineremoveemail=function (){
+		var removeemail_options= {};
+			removeemail_options.container = "removeemail-container";
+			removeemail_options.onSuccess = function(response) {
+			// On Success
+			console.log(response);
+			LRBCUX.interface.showMessage("Email has been successfully removed",5000);
+			};
+			removeemail_options.onError = function(response) {
+			// On Error
+				LRBCUX.interface.showMessage(response[0].Description,5000);
+			};
+			
+			LRObject.init("removeEmail",removeemail_options);
+	};	
+	
+	
+//End V2 Interfaces	
+	
+	LRBCUX.interface.injectSpinner=function(){
+		
+		if(!document.getElementById("fade")){
+			var div = document.createElement("div");
+				div.innerHTML = '<div class="lr-loading-frame"> <div class="lr-loading-box" style="background: url(//cdn.loginradius.com/demo/common/loading_spinner.gif) no-repeat center center;"> <span class="lr-loading-text-box"></span> </div> </div>	<div id="overshow" style="display:none"></div>';
+				div.id="fade";
+				div.style.cssText ="position: fixed; height: 100%; width: 100%; background-color:transparent; opacity: 0.8; top: 0px; left: 0px; display:none;";
+				document.body.appendChild(div);
+	
+		}
+		
+	}
+	
+	LRBCUX.interface.DisplayAuth=function (){
+		
+		LRBCUX.interface.injectSpinner();
+		
+		LRObject.$hooks.register('socialLoginFormRender',function(){
+			LoginRadiusBCUX.interface.toggleform('additional');
+					
+			document.getElementById("fade").style.display = 'none';
+		});
+		
+		document.getElementById("authcontainer").style.display = 'block';
+		LRObject.util.ready(function() {
+		
+				LRBCUX.interface.definelogin();
+				
+				LRBCUX.interface.defineregister();
+				
+				LRBCUX.interface.definesocial();
+				
+				LRBCUX.interface.defineforgot();
+				
+				LRBCUX.interface.defineverify();
+				
+				LRBCUX.interface.definereset();
+				var locCheck=window.location.href
+				if(locCheck.includes("vtype=reset")){
+					LRBCUX.interface.toggleform('forgot');
+					
+				 document.getElementById("forgotpassword-div").style.display = 'none';	
+				}
+				
+			});
+		};
+	
+	LRBCUX.interface.hideforms=function (){
+		 document.getElementById("lr-login-container").style.display = 'none';
+		 document.getElementById("lr-fp-container").style.display = 'none';
+		 document.getElementById("lr-reg-container").style.display = 'none';
+		 document.getElementById("lr-additional-container").style.display = 'none';
+		
+	};
+	LRBCUX.interface.toggleform=function (action)
+		{
+			 switch (action) {
+					case 'register':				
+						LRBCUX.interface.hideforms();
+						 document.getElementById("lr-reg-container").style.display = 'block';
+						break; 
+				
+					case 'login':				
+						LRBCUX.interface.hideforms();
+						  document.getElementById("lr-login-container").style.display = 'block';
+						break; 
+						
+					case 'forgot':				
+						LRBCUX.interface.hideforms();
+						 document.getElementById("lr-fp-container").style.display = 'block';
+						break; 
+					case 'additional':				
+						LRBCUX.interface.hideforms();
+						 document.getElementById("lr-additional-container").style.display = 'block';
+						break; 	
+					default:
+						break;
+			 }
+		};
+		
+	return LRBCUX;
+})(document);
---
+++ bigcommerce-blueprint-package/assets/js/loginradius/LoginRadiusMigHandler.js
@@ -0,0 +1,62 @@
+
+var lrRaasOptions=raasoption;
+var bcStoreName=storeName;
+var inc=0;
+var onSubmitCache;
+var saveData = function(){
+		var url="https://cloud-api.loginradius.com/sso/bigcommerce/api/validatepassword?apikey="+lrRaasOptions.apikey+"&store="+bcStoreName+"";
+		var password=$("#loginradius-raas-login-password").val();
+		var username=$("#loginradius-raas-login-emailid").val();
+		var validateData={"password":password,"emailid":username};
+		document.getElementById("fade").style="display:block";
+		$.ajax({
+		  type: "POST",
+		  url: url,
+		  data: validateData,
+		  dataType: "json",
+		  crossDomain: true,
+		  headers: {
+						'Access-Control-Allow-Origin': '*',
+						'Access-Control-Allow-Headers': 'Content-Type' 
+					},
+		  success: function(resultData){
+			  
+			  if(resultData.ErrorCode || resultData.errorCode||resultData.verified==false){
+					if(resultData.Description)
+						LoginRadiusBCUX.interface.showMessage(resultData.Description,10000);
+					if(resultData.description)
+						LoginRadiusBCUX.interface.showMessage(resultData.description,10000);
+					document.getElementById("fade").style="display:none";
+			  }else{
+				$('#loginradius-raas-submit-Login').parents('form:first').removeAttr('onsubmit');
+				$('#loginradius-raas-submit-Login').parents('form:first')[0].onsubmit=onSubmitCache;
+				$('#loginradius-raas-submit-Login').click();
+				
+			  }
+		  },
+		  error:function(result){
+			LoginRadiusBCUX.interface.showMessage(JSON.stringify(result),10000);
+		  }
+	});
+}
+
+var await=function(){
+		setTimeout(function(){ hideSubmit() }, 50);
+	}
+var hideSubmit=function(){
+	if($('#loginradius-raas-submit-Login').length)
+		{
+			inc=0;
+			onSubmitCache=$('form[name="loginradius-raas-login"]')[0].onsubmit;
+			$('#loginradius-raas-submit-Login').parents('form:first').attr("onsubmit","saveData();return false;");
+			
+		}
+			
+	else{
+		inc++;
+		if(inc<=200){
+			await();
+		}
+	}
+}
+await();
