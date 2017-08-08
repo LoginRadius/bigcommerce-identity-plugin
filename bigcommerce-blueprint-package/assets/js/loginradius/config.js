3a7255d56e225e6efcc6ffaac3ec4b097f2db033
Karl-LoginRadius <feorn16@gmail.com>
2017-08-08 09:58:24 -0700

    update config object

--- bigcommerce-blueprint-package/assets/js/loginradius/config.js
+++ bigcommerce-blueprint-package/assets/js/loginradius/config.js
@@ -1,18 +1,18 @@
 var storeName='<STORE NAME>';
 var enableSSO=true;
 
-var raasoption = {};
-	raasoption.apiKey = "<API Key>";
-	raasoption.appName = "<SITE Name>";
-	raasoption.sott="<SOTT>";	
-	raasoption.callbackUrl=window.location.href;	
-	raasoption.formValidationMessage = true;
-	raasoption.accessTokenResponse=true;
-	raasoption.verificationUrl = window.location.href.split("?")[0];
-	raasoption.resetPasswordUrl = window.location.href.split("?")[0];
-	raasoption.askEmailForUnverifiedProfileAlways=true;
-	raasoption.templateName = "loginradiuscustom_tmpl";
-	raasoption.hashTemplate = true; 
-	raasoption.askRequiredFieldForTraditionalLogin=true;
+var option = {};
+	option.apiKey = "<API Key>";
+	option.appName = "<SITE Name>";
+	option.sott="<SOTT>";	
+	option.callbackUrl=window.location.href;	
+	option.formValidationMessage = true;
+	option.accessTokenResponse=true;
+	option.verificationUrl = window.location.href.split("?")[0];
+	option.resetPasswordUrl = window.location.href.split("?")[0];
+	option.askEmailForUnverifiedProfileAlways=true;
+	option.templateName = "loginradiuscustom_tmpl";
+	option.hashTemplate = true; 
+	option.askRequiredFieldForTraditionalLogin=true;
 	
-var LRObject= new LoginRadiusV2(raasoption);	
+var LRObject= new LoginRadiusV2(option);	
